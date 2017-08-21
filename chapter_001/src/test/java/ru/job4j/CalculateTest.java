290420a484176f39e984d7864f40147751b37c3d
Ryslan Zaharov <Ryslan8906137@yandex.ru>
2017-08-21 11:29:55 +0300

    task 4122

---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,35 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Ryslan Zaharov (mailto:Ryslan8906137@yandex.ru)
+* @version $Id$
+* @since 21.08.18
+*/
+public class CalculateTest {
+/**
+* Test add.
+*/
+@Test
+public void whenAddOneToOneThenTwo() {
+ByteArrayOutputStream out = new ByteArrayOutputStream();
+System.setOut(new PrintStream(out));
+Calculate.main(null);
+assertThat(
+out.toString(),
+is(
+String.format(
+"Hello World%s",
+System.getProperty("line.separator")
+)
+)
+);
+}
+}

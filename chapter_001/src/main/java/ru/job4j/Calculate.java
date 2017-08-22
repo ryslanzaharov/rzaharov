31c77f0acdceb880de57430a97033ee080683477
Ryslan Zaharov <Ryslan8906137@yandex.ru>
2017-08-22 12:44:38 +0300

    task 4417

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,16 +1,16 @@
 package ru.job4j;
 
-	/**
-	* Класс вывода строки в консоль.
-	* @author Ryslan Zaharov (mailto:Ryslan8906137@yandex.ru).
-	* @version 01.
-	* @since 18.08.17.
-	*/
+/**
+ * Класс вывода строки в консоль.
+ * @author Ryslan Zaharov (mailto:Ryslan8906137@yandex.ru).
+ * @version 01.
+ * @since 18.08.17.
+ */
 public class Calculate {
 	/**
-	*Конструктор, вывод строки в консоль.
-	* @param args arg.
-	*/
+	 *Конструктор, вывод строки в консоль.
+	 * @param args arg.
+	 */
 	public static void main(String[] args) {
         System.out.println("Hello World");
     }

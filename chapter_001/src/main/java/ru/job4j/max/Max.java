03441dff215e7a4a316687c9fc61de7b6a3a8c54
Ryslan Zaharov <Ryslan8906137@yandex.ru>
2017-08-26 21:04:40 +0300

    task 190

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -4,4 +4,10 @@ public class Max {
     public int max(int first, int second){
         return first>second?first:second;
     }
+
+    public int max(int first, int second, int third) {
+        first = max(first,second);
+        second = max(first,third);
+        return second;
+    }
 }

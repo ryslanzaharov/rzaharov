9a7ed1111288debfbbd106b367e83770335ef72d
Ryslan Zaharov <Ryslan8906137@yandex.ru>
2017-08-29 22:56:59 +0300

    task 195

---
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+public class BubbleSort {
+    public int[] sort(int[] array) {
+        for (int i = 0; i <= array.length-1; i++) {
+            for (int n = 0; n <= array.length-1; n++) {
+                if (array[i] < array[n]) {
+                    int a = array[n];
+                    array[n] = array[i];
+                    array[i] = a;
+                }
+            }
+        }
+        return array;
+    }
+}

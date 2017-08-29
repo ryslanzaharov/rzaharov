a4d064eea5044d2547166c26a2df2f582f60f41d
Ryslan Zaharov <Ryslan8906137@yandex.ru>
2017-08-29 22:59:18 +0300

    task 195

--- chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
+++ chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
@@ -8,7 +8,6 @@ import static org.junit.Assert.assertThat;
 public class BubbleSortTest {
     @Test
     public void whenSortArrayWithTenElementsThenSortedArray() {
-        //напишите здесь тест, проверяющий сортировку массива из 10 элементов методом пузырька, например {1, 5, 4, 2, 3, 1, 7, 8, 0, 5}.
         BubbleSort bubbleSort = new BubbleSort();
         int[] array = new int[]{1, 5, 4, 2, 3, 1, 7, 8, 0, 5};
         int[] result = bubbleSort.sort(array);

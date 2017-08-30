af4e01e92fd9bde1244beac66154383799eee30f
Ryslan Zaharov <Ryslan8906137@yandex.ru>
2017-08-30 18:28:28 +0300

    task 195

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -3,12 +3,12 @@ package ru.job4j.array;
 public class BubbleSort {
 
     public int[] sort(int[] array) {
-        for (int i = 0; i <= array.length-1; i++) {
-            for (int n = 0; n <= array.length-1; n++) {
-                if (array[i] < array[n]) {
-                    int a = array[n];
-                    array[n] = array[i];
-                    array[i] = a;
+        for (int i = array.length-1; i > 0; i--) {
+            for (int n = 0; n < i; n++) {
+                if (array[n+1] < array[n]) {
+                    int m = array[n+1];
+                    array[n+1] = array[n];
+                    array[n] = m;
                 }
             }
         }

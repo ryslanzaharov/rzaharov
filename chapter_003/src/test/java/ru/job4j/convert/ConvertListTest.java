330246cd50b701ea2999f4717946e36bfe45bd44
Ryslan Zaharov <Ryslan8906137@yandex.ru>
2017-10-04 16:39:18 +0300

    task 10035

--- chapter_003/src/test/java/ru/job4j/convert/ConvertListTest.java
+++ chapter_003/src/test/java/ru/job4j/convert/ConvertListTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ConvertListTest {
@@ -15,13 +16,7 @@ public class ConvertListTest {
         int[][] array = {{1, 2, 3}, {4, 5, 6}, {7, 0 ,0}};
         List<Integer> result = convertList.toList(array);
         List<Integer> expected = new ArrayList<>();
-        expected.add(1);
-        expected.add(2);
-        expected.add(3);
-        expected.add(4);
-        expected.add(5);
-        expected.add(6);
-        expected.add(7);
+        expected.addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 0, 0));
         assertThat(result, is(expected));
     }
 
@@ -29,14 +24,7 @@ public class ConvertListTest {
     public void whenToArray() {
         ConvertList convertList = new ConvertList();
         List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(null);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-        list.add(6);
-        list.add(7);
+        list.addAll(Arrays.asList(1, 2, null, 3, 4, 5, 6, 7));
         int[][] result = convertList.toArray(list, 3);
         int[][] expected = {{1, 2, 3}, {4, 5, 6}, {7, 0 ,0}};
         assertThat(result, is(expected));

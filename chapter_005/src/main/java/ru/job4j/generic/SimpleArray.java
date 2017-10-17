61803998a1a22670caac48593edc8ca7d6cb22df
Ryslan Zaharov <Ryslan8906137@yandex.ru>
2017-10-17 20:44:52 +0300

    task 156

---
+++ chapter_005/src/main/java/ru/job4j/generic/SimpleArray.java
@@ -0,0 +1,48 @@
+package ru.job4j.generic;
+
+public class SimpleArray<T> {
+
+    private Object[] objects;
+    private int index = 0;
+
+    public SimpleArray(int size) {
+        this.objects = new Object[size];
+    }
+
+    public Object[] getObjects() {
+        return objects;
+    }
+
+    public void add(T value) {
+        if (this.index < objects.length)
+        this.objects[index++] = value;
+        else
+            throw new ArrayIndexOutOfBoundsException(String.format("error in add"));
+    }
+
+    public void update(int position, T value) {
+        if (position < objects.length && position >= 0)
+        this.objects[position] = value;
+        else
+            throw new ArrayIndexOutOfBoundsException(String.format("error in update"));
+    }
+
+    public Object[] delete(int position) {
+        Object [] objects1 = new Object[index - 1];
+        if (position < index) {
+            System.arraycopy(this.objects, 0, objects1, 0, position );
+            System.arraycopy(this.objects, position + 1, objects1, position, this.index - (position + 1));
+
+        }
+        else
+            throw new ArrayIndexOutOfBoundsException(String.format("error in delete"));
+        return objects1;
+    }
+
+    public T get(int position) {
+        if (position < this.objects.length)
+        return (T) this.objects[position];
+        else
+            throw new ArrayIndexOutOfBoundsException(String.format("error in get"));
+    }
+}

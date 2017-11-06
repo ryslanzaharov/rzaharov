8c3f57f85ae8f0107d7f64a09e19e28f1c565e36
Ryslan Zaharov <Ryslan8906137@yandex.ru>
2017-11-06 13:16:52 +0300

    task 1003

--- chapter_005/src/main/java/ru/job4j/map/User.java
+++ chapter_005/src/main/java/ru/job4j/map/User.java
@@ -15,4 +15,11 @@ public class User {
     }
 
 
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + children;
+        result = 31 * result + (birthday != null ? birthday.hashCode() : 0);
+        return result;
+    }
 }

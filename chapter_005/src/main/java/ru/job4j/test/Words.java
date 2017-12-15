f2bac600c79a4512104fa036523306bfe55cf235
Ryslan Zaharov <Ryslan8906137@yandex.ru>
2017-12-15 18:06:59 +0300

    task test

--- chapter_005/src/main/java/ru/job4j/test/Words.java
+++ chapter_005/src/main/java/ru/job4j/test/Words.java
@@ -1,10 +1,34 @@
 package ru.job4j.test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Words {
 
     private String word;
     public Words(String word) {
         this.word = word;
+    }
+
+    public boolean containsLetter(String word) {
+        boolean sameLetters = true;
+                if (this.word.length() == word.length()) {
+                        String[] array = this.word.split("");
+                        List<String> letters = new ArrayList<>(
+                                        Arrays.asList(array)
+                                        );
+                        for (String let : word.split("")) {
+                                if (letters.contains(let))
+                                       letters.remove(let);
+                                else
+                                   sameLetters = false;
+                            }
+                    }
+                else
+                    sameLetters = false;
+                return sameLetters;
+
     }
 
     @Override
@@ -14,7 +38,7 @@ public class Words {
 
         Words words = (Words) o;
 
-        return word != null ? word.equals(words.word) : words.word == null;
+        return word != null ? containsLetter(word) : words.word == null;
     }
 
     @Override
@@ -22,11 +46,14 @@ public class Words {
         int result = 0;
         if (word != null) {
         char[] ch = word.toCharArray();
-        int hash = word.charAt(0) - 96;
         int arraySize = ch.length;
         for (int i = 0; i < ch.length; i++) {
+            result += ch[i];
             result += ((ch[i] - 96) * 27) % arraySize;
-        }}
+        }
+        //   result = result % arraySize;
+        }
+
         return result;
     }
 }
--- chapter_005/src/test/java/ru/job4j/test/WordsTest.java
+++ chapter_005/src/test/java/ru/job4j/test/WordsTest.java
@@ -3,6 +3,8 @@ package ru.job4j.test;
 import org.junit.Test;
 
 import java.util.HashMap;
+import java.util.Map;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
@@ -10,11 +12,9 @@ public class WordsTest {
 
     @Test
     public void whenComparisonTest() {
-        Words words = new Words("value");
-        Words words1 = new Words("lueav");
-        HashMap<Words, Object> map = new HashMap<>();
-        map.put(words, new Object());
-        map.put(words1, new Object());
-        assertThat(map.size(), is(2));
+        Map<Words, Object> map = new HashMap<>();
+        map.put(new Words("value"), new Object());
+        map.put(new Words("lueav"), new Object());
+        assertThat(map.size(), is(1));
     }
 }

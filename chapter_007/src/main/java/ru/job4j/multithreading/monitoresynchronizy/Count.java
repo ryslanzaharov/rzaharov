99adeb3cc39dd50903d27ed3b8b90908a78d5084
Ryslan Zaharov <Ryslan8906137@yandex.ru>
2018-01-15 17:56:05 +0300

    task 1102

---
+++ chapter_007/src/main/java/ru/job4j/multithreading/monitoresynchronizy/Count.java
@@ -0,0 +1,17 @@
+package ru.job4j.multithreading.monitoresynchronizy;
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
+public class Count {
+    @GuardedBy("this")
+    private int value;
+
+    public synchronized void increment() {
+        this.value++;
+    }
+
+    public synchronized int get() {
+        return this.value;
+    }
+}

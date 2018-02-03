505f401faa144b9d0f2f7185600cc1cd8d530ceb
Ryslan Zaharov <Ryslan8906137@yandex.ru>
2018-02-03 21:26:26 +0300

    thread

---
+++ chapter_007/src/main/java/ru/job4j/multithreading/MatchCounter.java
@@ -0,0 +1,63 @@
+package ru.job4j.multithreading;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+
+class MatchCounter implements Callable<Integer> {
+    private File directory;
+    private String keyword;
+    private ExecutorService pool;
+    private int count;
+
+    public MatchCounter(File directory, String keyword, ExecutorService pool) {
+        this.directory = directory;
+        this.keyword = keyword;
+        this.pool = pool;
+    }
+
+    public Integer call() {
+        count = 0;
+        try {
+            File[] files = directory.listFiles();
+            List<Future<Integer>> results = new ArrayList<>();
+            for (File file : files)
+                if (file.isDirectory()) {
+                    MatchCounter counter = new MatchCounter(file, keyword, pool);
+                    Future<Integer> result = pool.submit(counter);
+                    results.add(result);
+                } else {
+                    if (search(file)) count++;
+                }
+            for (Future<Integer> result : results)
+                try {
+                    count += result.get();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+        } catch (InterruptedException e) {
+        }
+        return count;}
+/** * Осуществляет поиск заданного ключевого слова в файлах * брагаш file Файл для поиска ключевого слова * 6return true Возвращает логическое значение true, если * ключевое слово содержится в файле */
+
+    public boolean search(File file) {
+        try {
+             try (Scanner in = new Scanner(file, "UTF-8")) {
+                boolean found = false;
+                while (!found && in.hasNextLine()) {
+                    String line = in.nextLine();
+                    if (line.contains(keyword)) found = true;
+                }
+                return found;
+            }
+        }  catch(IOException e){
+            return false;
+        }
+    }
+}

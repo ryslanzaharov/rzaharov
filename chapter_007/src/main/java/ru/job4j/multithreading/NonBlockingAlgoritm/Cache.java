5173f69b27c96425a7748de0f3134a3101baacfb
Ryslan Zaharov <Ryslan8906137@yandex.ru>
2018-02-10 17:05:49 +0300

    erfj

---
+++ chapter_007/src/main/java/ru/job4j/multithreading/NonBlockingAlgoritm/Cache.java
@@ -0,0 +1,16 @@
+package ru.job4j.multithreading.NonBlockingAlgoritm;
+
+public class Cache {
+
+//    public boolean add() {
+//
+//    }
+//
+//    public boolean update() {
+//
+//    }
+//
+//    public boolean delete() {
+//
+//    }
+}

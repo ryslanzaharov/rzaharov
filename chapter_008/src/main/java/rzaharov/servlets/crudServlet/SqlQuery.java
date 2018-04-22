6fea35368a5984f0d6b0a768722f4c8c6cbf8fee
Ryslan Zaharov <Ryslan8906137@yandex.ru>
2018-04-22 18:40:56 +0300

    task 2512

---
+++ chapter_008/src/main/java/rzaharov/servlets/crudServlet/SqlQuery.java
@@ -0,0 +1,22 @@
+package rzaharov.servlets.crudServlet;
+
+/**
+ * Класс sql запросов.
+ * @author Ryslan Zaharov (mailto:Ryslan8906137@yandex.ru).
+ * @version 01.
+ * @since 22.04.18.
+ */
+
+public class SqlQuery {
+
+    public static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS userdata(email VARCHAR(20) PRIMARY KEY," +
+            " name VARCHAR, login VARCHAR, date TIMESTAMP)";
+
+    public static final String INSERT = "INSERT INTO userdata(email, name, login, date) VALUES(?, ?, ?, ?)";
+
+    public static final String SELECT = "SELECT * FROM userdata WHERE email = ?";
+
+    public static final String UPDATE = "UPDATE userdata SET  email = ?, name = ?, login = ?, date = ? WHERE email = ?";
+
+    public static final String DELETE = "DELETE FROM userdata WHERE email = ?";
+}

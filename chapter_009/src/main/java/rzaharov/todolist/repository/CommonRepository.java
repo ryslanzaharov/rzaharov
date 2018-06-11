44e0250f271a50b8fe4681dab2ae02980f8a01f6
Ryslan Zaharov <Ryslan8906137@yandex.ru>
2018-06-11 16:04:13 +0300

    task 3786

--- chapter_009/src/main/java/rzaharov/todolist/repository/CommonRepository.java
+++ chapter_009/src/main/java/rzaharov/todolist/repository/CommonRepository.java
@@ -2,6 +2,7 @@ package rzaharov.todolist.repository;
 
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import rzaharov.todolist.database.AllModels;
@@ -23,43 +24,61 @@ public abstract class CommonRepository<T> {
         this.dbManager = DBManager.getInstance();
     }
 
+//    public void execute(DBOperation dbOperation, T value) {
+//        try(Session session = this.dbManager.getSession()) {
+//            this.sessions = session;
+//            session.beginTransaction();
+//            dbOperation.execute(session, value);
+//            session.getTransaction().commit();
+//            session.close();
+//        } catch (HibernateException e) {
+//            log.error(e.getMessage(), e);
+//            sessions.getTransaction().rollback();
+//        }
+//    }
+
     public void execute(DBOperation dbOperation, T value) {
-        try(Session session = this.dbManager.getSession()) {
-            this.sessions = session;
-            session.beginTransaction();
+        final Session session = this.dbManager.getSession();
+        final Transaction tx = session.beginTransaction();
+        try {
             dbOperation.execute(session, value);
-            session.getTransaction().commit();
-            session.close();
         } catch (HibernateException e) {
             log.error(e.getMessage(), e);
-            sessions.getTransaction().rollback();
+            session.getTransaction().rollback();
+        } finally {
+            tx.commit();
+            session.close();
         }
     }
 
     public List<T> getAll(AllModels allModels) {
         List<T> list = null;
-        try(Session session = this.dbManager.getSession()) {
-            this.sessions = session;
-            session.beginTransaction();
+        final Session session = this.dbManager.getSession();
+        final Transaction tx = session.beginTransaction();
+        try {
             list = allModels.getAll(session);
-            session.getTransaction();
         } catch (HibernateException e) {
             log.error(e.getMessage(), e);
-            sessions.getTransaction().rollback();
+            session.getTransaction().rollback();
+        } finally {
+            tx.commit();
+            session.close();
         }
         return list;
     }
 
     public T getById(ID idOperation) {
+        final Session session = this.dbManager.getSession();
+        final Transaction tx = session.beginTransaction();
         T value = null;
-        try(Session session = this.dbManager.getSession()) {
-            this.sessions = session;
-            session.beginTransaction();
+        try {
             value = (T) idOperation.getById(session);
-            session.getTransaction().commit();
         } catch (HibernateException e) {
             log.error(e.getMessage(), e);
-            sessions.getTransaction().rollback();
+            session.getTransaction().rollback();
+        } finally {
+            tx.commit();
+            session.close();
         }
         return value;
     }

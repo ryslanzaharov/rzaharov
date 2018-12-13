bb099bca07a50e6abd6ee33849597643ce23450d
Ryslan Zaharov <Ryslan8906137@yandex.ru>
2018-12-13 16:00:10 +0300

    task 858

--- input-output/io/src/main/java/ByteStream.java
+++ input-output/io/src/main/java/ByteStream.java
@@ -1,27 +1,29 @@
 import org.apache.commons.io.IOUtils;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 
 public class ByteStream {
 
-    public boolean isNumber(InputStream in) throws IOException {
+    public boolean isNumber(InputStream in) {
         boolean isEvening = false;
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
-        try {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
             Integer result = Integer.parseInt(br.readLine());
             if (result % 2 == 0)
                 isEvening = true;
-        } catch (Exception e) {
+        } catch (IOException e) {
             System.out.println("sorry enter numbers");
         }
         return isEvening;
     }
 
-    public static void main(String[] args) throws IOException{
-        InputStream inputStream = new BufferedInputStream(System.in);
-        ByteStream bs = new ByteStream();
-        System.out.println(bs.isNumber(inputStream));
+    public static void main(String[] args){
+        try(InputStream inputStream = new BufferedInputStream(System.in)) {
+            ByteStream bs = new ByteStream();
+            System.out.println(bs.isNumber(inputStream));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }

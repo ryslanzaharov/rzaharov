78fb81d8fcc61c1a0d5d0d69e428a3a7677406f0
Ryslan Zaharov <Ryslan8906137@yandex.ru>
2018-12-13 20:44:54 +0300

    task 859

--- input-output/io/src/main/java/Drop.java
+++ input-output/io/src/main/java/Drop.java
@@ -2,14 +2,16 @@ import java.io.*;
 
 public class Drop {
 
-    public void dropAbuses(InputStream in, OutputStream out, String[] abuse) {
+    public void dropAbuses(InputStream in, OutputStream out) {
 
         try(BufferedReader br = new BufferedReader(new InputStreamReader(in));
                 PrintStream ps = new PrintStream(out)
         ) {
-
-            String strs = br.readLine().replaceAll("abuse", "");
-            ps.print(strs);
+            String[] abuse = br.readLine().split(" ");
+            for (String str : abuse) {
+                str = str.replace("abuse", "");
+                ps.println(str);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -17,6 +19,7 @@ public class Drop {
 
     public static void main(String[] args) {
         Drop drop = new Drop();
-        drop.dropAbuses(System.in, System.out, new String[12]);
+        drop.dropAbuses(System.in, System.out);
+
     }
 }

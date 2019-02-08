8f5a6bb62777c595b03adc7c8ffb26066f76c89b
Ryslan Zaharov <Ryslan8906137@yandex.ru>
2019-02-08 13:15:45 +0300

    noobchain

--- noobchain/src/main/java/Block.java
+++ noobchain/src/main/java/Block.java
@@ -4,20 +4,18 @@ public class Block {
 
     public String hash;
     public String previousHash;
-    private String data; //our data will be a simple message.
-    private long timeStamp; //as number of milliseconds since 1/1/1970.
+    private String data;
+    private long timeStamp;
     private int nonce;
 
-    //Block Constructor.
     public Block(String data,String previousHash ) {
         this.data = data;
         this.previousHash = previousHash;
         this.timeStamp = new Date().getTime();
 
-        this.hash = calculateHash(); //Making sure we do this after we set the other values.
+        this.hash = calculateHash();
     }
 
-    //Calculate new hash based on blocks contents
     public String calculateHash() {
         String calculatedhash = StringUtil.applySha256(
                 previousHash +
@@ -27,13 +25,12 @@ public class Block {
         );
         return calculatedhash;
     }
-
+    //создаем хеш с уровне сложности difficulty
     public void mineBlock(int difficulty) {
-        String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0"
+        String target = new String(new char[difficulty]).replace('\0', '0');
         while(!hash.substring( 0, difficulty).equals(target)) {
             nonce ++;
             hash = calculateHash();
         }
-        System.out.println("Block Mined!!! : " + hash);
     }
 }
--- noobchain/src/main/java/NoobChain.java
+++ noobchain/src/main/java/NoobChain.java
@@ -8,51 +8,48 @@ public class NoobChain {
     public static int difficulty = 5;
 
     public static void main(String[] args) {
-        //add our blocks to the blockchain ArrayList:
 
         System.out.println(new String(new char[difficulty]).replace('\0', '0'));
 
-        blockchain.add(new Block("Hi im the first block", "0"));
-        System.out.println("Trying to Mine block 1... ");
+        blockchain.add(new Block("1 блок", "0"));
         blockchain.get(0).mineBlock(difficulty);
 
-        blockchain.add(new Block("Yo im the second block",blockchain.get(blockchain.size()-1).hash));
-        System.out.println("Trying to Mine block 2... ");
+        blockchain.add(new Block("2 блок",blockchain.get(blockchain.size()-1).hash));
         blockchain.get(1).mineBlock(difficulty);
 
-        blockchain.add(new Block("Hey im the third block",blockchain.get(blockchain.size()-1).hash));
-        System.out.println("Trying to Mine block 3... ");
+        blockchain.add(new Block("3 блок",blockchain.get(blockchain.size()-1).hash));
         blockchain.get(2).mineBlock(difficulty);
 
-        System.out.println("\nBlockchain is Valid: " + isChainValid());
+        System.out.println("\nДействителен ли блокчейн: " + isChainValid());
 
         String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
-        System.out.println("\nThe block chains : ");
+        System.out.println("\nБлокчейн : ");
         System.out.println(blockchainJson);
     }
+
 
     public static Boolean isChainValid() {
         Block currentBlock;
         Block previousBlock;
         String hashTarget = new String(new char[difficulty]).replace('\0', '0');
 
-        //loop through blockchain to check hashes:
+        //перебираем блок цепочку для проверки хешей
         for(int i=1; i < blockchain.size(); i++) {
             currentBlock = blockchain.get(i);
             previousBlock = blockchain.get(i-1);
-            //compare registered hash and calculated hash:
+            //сравнение зарегестрированного и вычисленного хеша
             if(!currentBlock.hash.equals(currentBlock.calculateHash()) ){
-                System.out.println("Current Hashes not equal");
+                System.out.println("зарегестрированный и вычисленный хеши не соответствуют");
                 return false;
             }
-            //compare previous hash and registered previous hash
+            //сравнение предыдущего хеша и зарегистрированного предыдущего хеша
             if(!previousBlock.hash.equals(currentBlock.previousHash) ) {
-                System.out.println("Previous Hashes not equal");
+                System.out.println("предыдущий хеш и зарегистрированный предыдущий хеши не соответствуют");
                 return false;
             }
-            //check if hash is solved
+            //проверка на уровень сложности
             if(!currentBlock.hash.substring( 0, difficulty).equals(hashTarget)) {
-                System.out.println("This block hasn't been mined");
+                System.out.println("Этот блок не соответсвует уровню сложности либо  не является зарегестрированным");
                 return false;
             }
         }

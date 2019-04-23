44e71c8c2514821217ca962c2b58a096405ce9e4
Ryslan Zaharov <Ryslan8906137@yandex.ru>
2019-04-23 17:18:22 +0300

    test

--- chapter_010/springTest/src/main/java/ru/rzaharov/UserDataRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.rzaharov;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserDataRepository extends CrudRepository<User, Integer> {
-}

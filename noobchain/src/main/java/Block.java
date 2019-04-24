da051e830bcda595f8a495e8603211c84e17b227
Руслан <ryslanzah@gmail.com>
2019-04-24 12:38:47 +0300

    block

--- noobchain/src/main/java/Block.java
+++ noobchain/src/main/java/Block.java
@@ -2,16 +2,17 @@ import java.util.Date;
 
 public class Block {
 
+    private String data;
     public String hash;
     public String previousHash;
-    private String data;
-    private long timeStamp;
+//    private long timeStamp;
     private int nonce;
+    private Date date;
 
-    public Block(String data,String previousHash ) {
+    public Block(String data, String previousHash) {
         this.data = data;
         this.previousHash = previousHash;
-        this.timeStamp = new Date().getTime();
+        this.date = new Date();
 
         this.hash = calculateHash();
     }
@@ -19,18 +20,30 @@ public class Block {
     public String calculateHash() {
         String calculatedhash = StringUtil.applySha256(
                 previousHash +
-                        Long.toString(timeStamp) +
+                        Long.toString(date.getTime()) +
                         Integer.toString(nonce) +
                         data
         );
         return calculatedhash;
     }
+
     //создаем хеш с уровне сложности difficulty
     public void mineBlock(int difficulty) {
         String target = new String(new char[difficulty]).replace('\0', '0');
-        while(!hash.substring( 0, difficulty).equals(target)) {
-            nonce ++;
+        while (!hash.substring(0, difficulty).equals(target)) {
+            nonce++;
             hash = calculateHash();
         }
+    }
+
+    @Override
+    public String toString() {
+        return " блок{"+ "\n" +
+                " данные='" + data + '\'' + "\n" +
+                " хеш код='" + hash + '\''  + "\n" +
+                " предыдущий хеш код='" + previousHash + '\'' + "\n" +
+                " индекс=" + nonce + '\''  + "\n" +
+                " время=" + date +  "\n" +
+                '}';
     }
 }
